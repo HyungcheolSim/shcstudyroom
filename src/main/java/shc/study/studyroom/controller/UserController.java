13b256741a19a82e57974b39bf457a29623e4d5c
HyungCheolSim <sim34122@gmail.com>
2023-01-19 22:41:58 +0900

    set logback for database and public code

--- src/main/java/shc/study/studyroom/controller/UserController.java
+++ src/main/java/shc/study/studyroom/controller/UserController.java
@@ -2,8 +2,9 @@ package shc.study.studyroom.controller;
 
 import lombok.RequiredArgsConstructor;
 
-import org.mybatis.logging.Logger;
-import org.mybatis.logging.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shc.study.studyroom.dto.User;
@@ -22,8 +23,12 @@ public class UserController {
 
     @GetMapping("/hello")
     public String test() {
-
-        throw new IllegalArgumentException("hello");
+        logger.trace("Trace Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("ERROR Level 테스트");
+        return "index";
         
     }
 
@@ -37,6 +42,7 @@ public class UserController {
         header.set("studyroomManager",userService.selectOneUser(2).getUserName());
         return ResponseEntity.ok(data);
     */
+        logger.debug("ResponseEntity<List<LinkedHashMap<Object, Object>>> findAll() 호출");
         return ResponseEntity.ok().body(userService.selectAllUser());
 
     }
--- src/main/java/shc/study/studyroom/service/UserServiceImpl.java
+++ src/main/java/shc/study/studyroom/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package shc.study.studyroom.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import shc.study.studyroom.dao.UserMapper;
 import shc.study.studyroom.dto.User;
@@ -11,34 +13,38 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
+    private final Logger logger= LoggerFactory.getLogger(this.getClass());
     private final UserMapper userMapper;
 
     @Override
     public List<LinkedHashMap<Object, Object>> selectAllUser() {
+        logger.info("List<LinkedHashMap<Object, Object>> selectAllUser()");
         return userMapper.showAllUser();
     }
 
     @Override
     public User selectOneUser(int userIdx) {
+        logger.info("User selectOneUser(int userIdx)"+ userIdx);
         return userMapper.selectUserByIdx(userIdx);
     }
 
     @Override
     public User addUser(User user) {
+        logger.info("User addUser(User user)"+ user);
         userMapper.insertUser(user);
         return userMapper.selectUserByIdx(user.getUserIdx());
     }
 
     @Override
     public User modUser(User user) {
+        logger.info("User modUser(User user)"+ user);
         userMapper.updateUser(user);
         return user;
     }
 
     @Override
     public User delUser(int userIdx) {
-
+        logger.info("User delUser(int userIdx)"+ userIdx);
         userMapper.deleteUser(userIdx);
         return userMapper.selectUserByIdx(userIdx);
     }
